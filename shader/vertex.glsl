7b7c3867a2ddcf9bcf722d4d32746115bae23df9
CharlieLei <lei_cheng2008@126.com>
2020-10-08 19:39:39 +0800

    完成基础光照练习4:实现一个Gouraud着色

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -2,11 +2,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
-out vec3 FragPos;
-out vec3 Normal;
-out vec3 LightPos;
+out vec4 aFragColor;
 
 uniform vec3 lightPos; // we now define the uniform in the vertex shader and pass the 'view space' lightpos to the fragment shader. lightPos is currently in world space.
+uniform vec3 lightColor;
+uniform vec3 objectColor;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -15,7 +15,27 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    FragPos = vec3(view * model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(view * model))) * aNormal;
-    LightPos = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
+    vec3 FragPos = vec3(view * model * vec4(aPos, 1.0));
+    vec3 Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    vec3 LightPos = vec3(view * vec4(lightPos, 1.0)); // Transform world-space light position to view-space light position
+
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(LightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // specular
+    float specularStrength = 10.5;
+    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    aFragColor = vec4(result, 1.0);
 }
