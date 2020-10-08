834eda69c928419db575dd22e128a7502588d13e
CharlieLei <lei_cheng2008@126.com>
2020-10-08 19:32:35 +0800

    完成基础光照练习3:在观察空间（而不是世界空间）中计算冯氏光照

--- main.cpp
+++ main.cpp
@@ -173,10 +173,6 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        float angle = 40.0f * glfwGetTime();
-        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
