1b5af82f0f1f460f2a22d4b6d6de7aa6b4bc5746
CharlieLei <lei_cheng2008@126.com>
2020-10-08 15:26:40 +0800

    完成摄像机练习2：自己的LookAt函数

--- Camera.cpp
+++ Camera.cpp
@@ -1,5 +1,29 @@
 #include "Camera.h"
 
+glm::mat4 myLookAt(glm::vec3 eyePos, glm::vec3 target, glm::vec3 worldUp)
+{
+    glm::vec3 front = glm::normalize(eyePos - target);
+    glm::vec3 right = glm::normalize(glm::cross(worldUp, front));
+    glm::vec3 up    = glm::normalize(glm::cross(right, front));
+    glm::mat4 rotation(1.0f);
+    rotation[0][0] = right.x;
+    rotation[1][0] = right.y;
+    rotation[2][0] = right.z;
+    rotation[0][1] = up.x;
+    rotation[1][1] = up.y;
+    rotation[2][1] = up.z;
+    rotation[0][2] = front.x;
+    rotation[1][2] = front.y;
+    rotation[2][2] = front.z;
+
+    glm::mat4 translation(1.0f);
+    translation[3][0] = -eyePos.x;
+    translation[3][1] = -eyePos.y;
+    translation[3][2] = -eyePos.z;
+
+    return rotation * translation;
+}
+
 Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
         : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM) {
     Position = position;
@@ -20,7 +44,8 @@ Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float u
 
 glm::mat4 Camera::GetViewMatrix()
 {
-    return glm::lookAt(Position, Position + Front, Up);
+//    return glm::lookAt(Position, Position + Front, Up);
+    return myLookAt(Position, Position + Front, WorldUp);
 }
 
 void Camera::ProcessKeyboard(Camera_Movement direction, float deltaTime)
