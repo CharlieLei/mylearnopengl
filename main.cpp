ab983e4ecd4db97edd1d410533163fada9fd37b0
CharlieLei <lei_cheng2008@126.com>
2020-10-09 10:45:11 +0800

    完成材质练习1

--- main.cpp
+++ main.cpp
@@ -176,13 +176,6 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
                 (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-        glm::vec3 lightColor;
-        lightColor.x = sin(glfwGetTime() * 2.0f);
-        lightColor.y = sin(glfwGetTime() * 0.7f);
-        lightColor.z = sin(glfwGetTime() * 1.3f);
-        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
-
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
         lightingShader.setMat4("model", model);
@@ -190,13 +183,13 @@ int main()
         lightingShader.setMat4("projection", projection);
         lightingShader.setVec3("viewPos", camera.Position);
         lightingShader.setVec3("light.position", lightPos);
-        lightingShader.setVec3("light.ambient",  ambientColor);
-        lightingShader.setVec3("light.diffuse",  diffuseColor); // 将光照调暗了一些以搭配场景
+        lightingShader.setVec3("light.ambient",  1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("light.diffuse",  1.0f, 1.0f, 1.0f); // 将光照调暗了一些以搭配场景
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-        lightingShader.setFloat("material.shininess", 32.0f);
+        lightingShader.setVec3("material.ambient", 0.0,	0.1,	0.06);
+        lightingShader.setVec3("material.diffuse",  0.0,	0.50980392,	0.50980392);
+        lightingShader.setVec3("material.specular", 0.50196078,	0.50196078,	0.50196078);
+        lightingShader.setFloat("material.shininess", 0.25 * 128.0);
 
         // render the cube
         glBindVertexArray(cubeVAO);
